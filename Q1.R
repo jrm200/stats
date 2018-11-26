9f786654eb41e47b2d6c3fcaa5f22a844e7becaa
jrm200 <43645609+jrm200@users.noreply.github.com>
2018-11-26 16:05:13 +0000

    Delete Q1.R

--- Q1.R
+++
@@ -1,9 +0,0 @@
-ratlikelihood <- function (theta) {
-  #set up variables of data to compute likelihood
-  rats2 <- rats[which(rats$status==1),,]
-  t <- rats2$time
-  
-  loglik <- sum(log(dweibull(t, shape=1/exp(theta[3]), scale=exp(theta[1] + theta[2]))))
-
-  loglik
-}
