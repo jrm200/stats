6d655d9bbd73d71e2e8b63311bb9032f25e41323
jrm200 <43645609+jrm200@users.noreply.github.com>
2018-11-28 12:16:38 +0000

    Update Q1.R

--- Q1.R
+++ Q1.R
@@ -5,10 +5,22 @@ rats<- read.table("http://people.bath.ac.uk/kai21/ASI/rats_data.txt")
 ratlikelihood <- function (theta) {
   #create subset of data since for the purposes of calculating the optimal theta since when status = 0,
   #the survival function does not depend on theta. Need to optimise this function using optim.
-  t <- rats[which(rats$status==1),,]$time
+  t1 <- rats[which(rats$status==1),,]$time
+  rx1 <- rats[which(rats$status==1),,]$rx
   
   #compute the negative log likelihood
-  loglik <- -sum(log(dweibull(t, shape=1/exp(theta[3]), scale=exp(theta[1] + theta[2]))))
+  loglik1 <- -sum(log(dweibull(t1, shape=1/exp(theta[3]), scale=exp(theta[1] + theta[2]*rx1))))
+  
+  t0 <- rats[which(rats$status==0),,]$time
+  rx0 <- rats[which(rats$status==0),,]$rx 
+  
+  a <- 1/exp(theta[3])
+  b <- exp(theta[1] + theta[2]*rx0)
+  
+  loglik2 <- sum((t0/b)^a)
+  
+  loglik <- loglik1 + loglik2
+  
   loglik
 }
 
@@ -21,6 +33,9 @@ theta <- c(0,0,0)
 beta0 <- theta[1]
 beta1 <- theta[2]
 logsig <- theta[3]
+beta0var <- 0
+beta1var <- 0
+logsigvar <- 0
 
 for (i in 0:1000) {
   theta[1] <- i*0.005
@@ -47,3 +62,18 @@ for (i in 0:1000) {
 }
 
 plot(logsig, logsigvar, type="l") #estimate optimal logsigma to be ~1.75
+
+theta <- c(4.5,4.5,1.75)
+ratlik <- optim(theta, ratlikelihood, hessian=TRUE, method="Nelder-Mead")
+hess <- solve(ratlik$hessian)
+stderr <- sqrt(diag(hess))
+# this needs work dont use normal distribution
+CI <- c(ratlik$par[2]-1.96*stderr[2], ratlik$par[2]+1.96*stderr[2])
+
+
+x1 <- seq(from=0,to=200,by=0.5)
+y1 <- dweibull(x1, shape = 1/exp(ratlik$par[3]), scale = exp(ratlik$par[1] + ratlik$par[2]))
+plot(x1,y1,type="l")
+x0 <- seq(from=0,to=200,by=0.5)
+y0 <- dweibull(x0, shape = 1/exp(ratlik$par[3]), scale = exp(ratlik$par[1]))
+plot(x0,y0,type="l")
