dd9ae66f1d8e4f87e486768c88ac6c1c1543250d
jrm200 <43645609+jrm200@users.noreply.github.com>
2018-11-30 11:16:03 +0000

    Update Q4.R

--- Q4.R
+++ Q4.R
@@ -1,32 +1,54 @@
 rats<- read.table("http://people.bath.ac.uk/kai21/ASI/rats_data.txt")
 
-sigma <- 1
-
-b <- rnorm(50, sd = sigma)
-
-X <- model.matrix(~ rx, rats)
+#note now theta = (beta0, beta1, log(sigma), log(sigmab))
+X <- model.matrix(~ rx, rats) #define X matrix
 
 rats$litter <- factor(rats$litter)
+Z <- model.matrix(~ litter - 1, rats) #define Z matrix
 
-Z <- model.matrix(~ litter - 1, rats)
-
-y <- rats$time
+b <- rep(0, ncol(Z)) #set arbitrary b
+y <- rats$time #define y vector
+theta <- c(0,0,0,0) #arbitrary
 
 lfyb <- function (theta, y, b, X, Z) {
-  beta <- c(theta[1], theta[2])
-  eta <- as.numeric(X%*%beta + Z%*%b)
-  loglik <- 0
-  k <- 1/exp(theta[3]) #shape
-  lambda <- exp(eta)
+  #computes joint log density of y and b
+  #first compute conditional density of y given b
   
-  for (i in 1:150){
+  beta <- c(theta[1:2]) #define beta vector
+  eta <- as.numeric(X%*%beta + Z%*%b) #define eta vector
+  loglik <- 0 #initialise loglik
+  k <- 1/exp(theta[3]) #shape
+  lambda <- exp(eta) #scale vector
+  
+  #log conditional likelihood of y given b
+  for (i in 1:length(y)) {
     if (rats$status[i] == 0){
-      loglik <- loglik + (y[i]/lambda[i])^k
+      loglik[i] <- (y[i]/lambda[i])^k
     }
-    else{
-      loglik <- loglik - log(dweibull(y[i], shape = k, scale = lambda[i]))
+    else {
+      loglik[i] <- -(dweibull(y[i], shape = k, scale = lambda[i], log=TRUE))
     }
   }
+  lfy_b <- sum(loglik)
   
-  loglik
+  #log likelihood of b
+  sig.b <- exp(theta[4])
+  lfb <- -sum(dnorm(b, 0, sig.b, log=TRUE))
+  
+  #joint density is (log likelihood of y given b) + (log likelihood of b)
+  lf <- lfy_b + lfb
+  #output lf
+  lf
+}
+
+#STARTED BUT NEEDS WORK
+lal <- function (theta, y, X, Z) {
+  #compute the negative log likelihood of theta using the laplace approximation
+  sig.b <- exp(theta[4])
+  beta <- c(theta[1:2])
+  eta <- X%*%beta
+  b <- rep(0,ncol(Z))
+  
+  lf <- lfyb(theta, y, b, X, Z)
+  optim(par=b, fn=lfyb)
 }
