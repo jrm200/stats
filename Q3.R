646bb6cc27f74da08c8fae14026d89a2bbfee4ac
jrm200 <43645609+jrm200@users.noreply.github.com>
2018-12-09 14:21:17 +0000

    Update Q3.R

--- Q3.R
+++ Q3.R
@@ -1,31 +1,21 @@
 #read in the data
 rats<- read.table("http://people.bath.ac.uk/kai21/ASI/rats_data.txt")
 
-#define likelihood function to minimise
+#define negative log likelihood function to minimise
 nll.llogistic <- function (theta) {
-  #create subsets of data since for the likelihood function since the contribution to the likelihood varies
-  #depending on the 'status' variable
-  t0 <- rats[which(rats$status==0),,]$time
-  rx0 <- rats[which(rats$status==0),,]$rx
-  t1 <- rats[which(rats$status==1),,]$time
-  rx1 <- rats[which(rats$status==1),,]$rx
-  
-  #compute the negative log likelihood
+  #initialise variables from dataset
+  t <- rats$time
+  s <- rats$status
+  rx <- rats$rx
+  #define shape and scale of log-logistic distribution
   k <- 1/exp(theta[3]) #shape
-  lambda <- exp(theta[1] + theta[2]*rx1) #scale
-  
-  loglik1 <- -sum(log((k/lambda)*(t1/lambda)^(k-1)/(1+(t1/lambda)^k)^2))
-
-  lambda <- exp(theta[1] + theta[2]*rx0) #redefine b for new rx vector
-  
-  loglik2 <- sum(log(1+(t0/lambda)^k))
-  
-  loglik <- loglik1 + loglik2
-  
+  lambda <- exp(theta[1] + theta[2]*rx) #scale
+  #compute negative log likelihood
+  loglik <- -sum(log(((k/lambda)*(t/lambda)^(k-1)/(1+(t/lambda)^k)^2)^s*(1/(1+(t/lambda)^k))^(1-s)))
   loglik
 }
 
-#to pick initial point, vary paramaters of theta individually and fix the others to find a rough minimum
+#to pick initial point, vary paramaters of theta individually and fix the others to find an approximate minimum
 
 #initialize variables
 theta <- c(0,0,0)
@@ -41,48 +31,33 @@ for (i in 0:1000) {
   beta0[i+1] <- theta[1]
   beta0var[i+1] <- nll.llogistic(theta)
 }
+plot(beta0, beta0var, type="l") #estimate optimal beta0 to be ~ 5.75
 
-plot(beta0, beta0var, type="l") #estimate optimal beta0 to be ~5.75
-
-theta <- c(0,0,0)
+theta <- c(5.75,0,0)
 for (i in 0:1000) {
-  theta[2] <- 2.5 + i*0.005
+  theta[2] <- -2.5 + i*0.005
   beta1[i+1] <- theta[2]
   beta1var[i+1] <- nll.llogistic(theta)
 }
+plot(beta1, beta1var, type="l") #estimate optimal beta1 to be ~ -0.5
 
-plot(beta1, beta1var, type="l") #estimate optimal beta1 to be ~5.25
-
-theta <- c(0,0,0)
+theta <- c(5.75,-0.5,0)
 for (i in 0:1000) {
-  theta[3] <- i*0.005
+  theta[3] <- -2.5 + i*0.005
   logsig[i+1] <- theta[3]
   logsigvar[i+1] <- nll.llogistic(theta)
 }
+plot(logsig, logsigvar, type="l") #estimate optimal logsigma to be ~ -0.5
 
-plot(logsig, logsigvar, type="l") #estimate optimal logsigma to be ~2.25
-
-theta <- c(5.75,5.5,2.25)
-ratlik <- optim(theta, nll.llogistic, hessian=TRUE, method="Nelder-Mead")
-llogisticapprox <- ratlik$par
+theta <- c(5.75,-0.5,-0.5)
+nll.optim <- optim(theta, nll.llogistic, hessian=TRUE, method="BFGS")
+theta.llog <- nll.optim$par #optimal theta
+hess <- nll.optim$hessian
 
 #then to find the standard error, take the sqrt of the diag of the inverse hessian
-hess <- solve(ratlik$hessian)
-stderr <- sqrt(diag(hess))
+se <- sqrt(diag(solve(hess)))
 
 #95% CI = parameter estimate +- ~1.96*stderr
-CI <- c(ratlik$par[2]+ qnorm(0.025)*stderr[2], ratlik$par[2]+qnorm(0.975)*stderr[2])
+CI <- c(theta.llog[2] + qnorm(0.025)*se[2], theta.llog[2] + qnorm(0.975)*se[2])
 
-#a <- 1/exp(ratlik$par[3]) #shape
-#b <- exp(ratlik$par[1] + ratlik$par[2]) #scale (received treatment)
-
-#x1 <- seq(from=0,to=200,by=0.5)
-#y1 <- (a/b)*(x1/b)^(a-1)/(1+(x1/b)^a)^2
-#plot(x1,y1,type="l")
-
-#b <- exp(ratlik$par[1]) #redefine scale (no treatment)
-#x0 <- seq(from=0,to=200,by=0.5)
-#y0 <- (a/b)*(x0/b)^(a-1)/(1+(x0/b)^a)^2
-#plot(x0,y0,type="l")
-
-#plots appear to show that rats which received medicine died sooner.................
+#appears to show that tumours appeared sooner in rats which received treatment
