91fdc95ae01b65348c9b65d6203485ff9fcd91b6
jrm200 <43645609+jrm200@users.noreply.github.com>
2018-12-09 14:20:38 +0000

    Update Q1.R

--- Q1.R
+++ Q1.R
@@ -1,29 +1,21 @@
 #read in the data
 rats<- read.table("http://people.bath.ac.uk/kai21/ASI/rats_data.txt")
 
-#define likelihood function to minimise
+#define negative log likelihood function to minimise
 nll.weibull <- function (theta) {
-  #create subset of data since for the purposes of calculating the optimal theta since when status = 0,
-  #the survival function does not depend on theta. Need to optimise this function using optim.
-  t1 <- rats[which(rats$status==1),,]$time
-  rx1 <- rats[which(rats$status==1),,]$rx
-  t0 <- rats[which(rats$status==0),,]$time
-  rx0 <- rats[which(rats$status==0),,]$rx
-  
-  #compute the negative log likelihood
+  #initialise variables from dataset
+  s <- rats$status
+  rx <- rats$rx
+  t <- rats$time
+  #define shape and scale of Weibull distribution
   k <- 1/exp(theta[3]) #shape
-  lambda <- exp(theta[1] + theta[2]*rx0) #scale
-  
-  loglik1 <- -sum(dweibull(t1, shape=1/exp(theta[3]), scale=exp(theta[1] + theta[2]*rx1), log=TRUE))
-  
-  loglik2 <- sum((t0/lambda)^k)
-  
-  loglik <- loglik1 + loglik2
-  
+  lambda <- exp(theta[1] + theta[2]*rx) #scale
+  #compute negative log likelihood
+  loglik <- -sum(log(((k/lambda)*(t/lambda)^(k-1)*exp(-(t/lambda)^k))^s*(exp(-(t/lambda)^k))^(1-s)))
   loglik
 }
 
-#to pick initial point, vary paramaters of theta individually and fix the others to find a rough minimum
+#to pick initial theta, vary paramaters of theta individually and fix the others to find an approximate minimum
 
 #initialize variables
 theta <- c(0,0,0)
@@ -61,26 +53,14 @@ for (i in 0:1000) {
 plot(logsig, logsigvar, type="l") #estimate optimal logsigma to be ~2.75
 
 theta <- c(5.75,5.25,2.75)
-ratlik <- optim(theta, nll.weibull, hessian=TRUE, method="Nelder-Mead")
-weibullapprox <- ratlik$par
+weibull.optim <- optim(theta, nll.weibull, method="BFGS", hessian=TRUE)
+theta.weibull <- weibull.optim$par
+hess <- solve(weibull.optim$hessian)
 
-#then to find the standard error, take the sqrt of the diag of the inverse hessian
-hess <- solve(ratlik$hessian)
-stderr <- sqrt(diag(hess))
+#then to find the standard error, take the square root of the diagonal of the inverse hessian
+se <- sqrt(diag(hess))
 
 #95% CI = parameter estimate +- ~1.96*stderr
-CI <- c(ratlik$par[2]+qnorm(0.025)*stderr[2], ratlik$par[2]+qnorm(0.975)*stderr[2])
+CI.norand <- c(theta.weibull[2]+qnorm(0.025)*se[2], theta.weibull[2]+qnorm(0.975)*se[2])
 
-#a <- 1/exp(ratlik$par[3]) #shape
-#b <- exp(ratlik$par[1] + ratlik$par[2]) #scale (received treatment)
-
-#x1 <- seq(from=0,to=200,by=0.5)
-#y1 <- dweibull(x1, shape = a, scale = b)
-#plot(x1,y1,type="l")
-
-#b <- exp(ratlik$par[1]) #redefine scale (no treatment)
-#x0 <- seq(from=0,to=200,by=0.5)
-#y0 <- dweibull(x0, shape = a, scale = b)
-#plot(x0,y0,type="l")
-
-#plots appear to show that rats which received medicine died sooner.................
+#appears to show that tumours appeared sooner in rats which received treatment
