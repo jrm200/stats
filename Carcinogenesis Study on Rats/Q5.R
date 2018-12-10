353cc2a8a7c6dff23006c01321aabdbb82c67462
jrm200 <43645609+jrm200@users.noreply.github.com>
2018-12-10 14:46:46 +0000

    Update Q5.R

--- Carcinogenesis Study on Rats/Q5.R
+++ Carcinogenesis Study on Rats/Q5.R
@@ -1,47 +1,55 @@
-lal <- function (theta, y, X, Z) {
-  #compute the negative log likelihood of theta using the laplace approximation
-  ratz <- optim(b, fn=lfyb, theta=theta, y=y, X=X, Z=Z, method="BFGS", hessian=TRUE)
-  bhat <- ratz$par
-  hess <- ratz$hessian
-  lapprox <- -(length(bhat)/2)*log(2*pi) + (1/2)*sum(log(diag(hess))) + lfyb(b=bhat, y, theta, X, Z)
-  #print(diag(hess))
-  lapprox
-}
-
 log.posterior <- function (theta, times = y, X.mat=X, Z.mat=Z) {
   #log likelihood
-  l.lik <- -lal(theta, times, X.mat, Z.mat) + dexp(x=theta[4], rate=5, log=TRUE)
+  prior <- dexp(x=exp(theta[4]), rate=5, log=TRUE)
+  prior[prior==-Inf] <- -100
+  l.lik <- -lal(theta, times, X.mat, Z.mat) + prior
   l.lik
 }
 
-n.rep <- 10
-sigma.prop <- 0.5
+n.rep <- 10000
+sigma.prop <- 0.1275 #began with 0.05, ar = 0.538. Tuned to 0.1275(ish) by varying sigma until ar ~ 0.23
 
 MH <- function (theta0, sigma.prop, n.rep) {
- theta.vals <- matrix(0, n.rep, 4) #matrix to save generated values
- theta.vals[1,] <- theta0
- lp0 <- log.posterior(theta.vals[1,])
- alpha <- rep(0, n.rep)
- 
- for (i in 2:n.rep) {
-   current_theta <- theta.vals[i-1,]
-   proposed_theta <- current_theta + rnorm(4, sd=sigma.prop)/10
-   lp1 <- log.posterior(proposed_theta)
-   acc <- exp(min(0, lp1-lp0))
-   test.value <- runif(1)
-   if (lp1 != -Inf && test.value <= acc) {
-    theta.vals[i,] <- proposed_theta
-    lp0 <- lp1
-    alpha[i] <- 1
+  theta.vals <- matrix(0, n.rep, 4) #matrix to save generated values
+  theta.vals[1,] <- theta0
+  lp0 <- log.posterior(theta.vals[1,])
+  alpha <- rep(0, n.rep)
+  
+  for (i in 2:n.rep) {
+    current_theta <- theta.vals[i-1,]
+    proposed_theta <- current_theta + rnorm(4, sd=sigma.prop)
+    lp1 <- log.posterior(proposed_theta)
+    acc <- exp(min(0, lp1-lp0))
+    test.value <- runif(1)
+    if (test.value <= acc) {
+      theta.vals[i,] <- proposed_theta
+      lp0 <- lp1
+      alpha[i] <- 1
     } else {
-    theta.vals[i,] <- current_theta
-    lp1 <- lp0
-   }
- }
- accept.rate <- sum(alpha)/n.rep
- list(theta=theta.vals, accept.rate=accept.rate)
+      theta.vals[i,] <- current_theta
+      lp1 <- lp0
+    }
+  }
+  accept.rate <- sum(alpha)/n.rep
+  list(theta=theta.vals, accept.rate=accept.rate, alpha=alpha)
 }
 
-mh <- MH(theta0 = thetahat$par, sigma.prop, n.rep)
+theta0 <- c(1,0,0,0)
+mh <- MH(theta0, sigma.prop, n.rep)
 theta <- mh$theta
 accept.rate <- mh$accept.rate
+
+acf(theta[,4], lag.max=1000) #looks like the 'stickiest' parameter is theta[4], acf = 0 after about 200 iterations, effective
+acf(theta[,3], lag.max=200) #sample size = n.rep/200
+acf(theta[,2], lag.max=200)
+acf(theta[,1], lag.max=200)
+
+theta <- theta[1001:n.rep,]
+
+par(mfrow=c(3,2),mar=c(4,4,1,1))
+plot(theta[,1],theta[,2],xlab="B0",ylab="B1",pch=20,cex=1)
+plot(theta[,1],theta[,3],xlab="B0",ylab="log(sig)",pch=20,cex=1)
+plot(theta[,1],theta[,4],xlab="B0",ylab="log(sig.b)",pch=20,cex=1)
+plot(theta[,2],theta[,3],xlab="B1",ylab="log(sig)",pch=20,cex=1)
+plot(theta[,2],theta[,4],xlab="B1",ylab="log(sig.b)",pch=20,cex=1)
+plot(theta[,3],theta[,4],xlab="log(sig)",ylab="log(sig.b)",pch=20,cex=1)
